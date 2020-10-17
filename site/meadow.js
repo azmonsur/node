7ec675f30a127eadd6380503de86389716dd40be
Monsur <donkenny03@gmail.com>
2020-10-17 12:36:30 +0100

    Many files created and added

--- site/meadow.js
+++ site/meadow.js
@@ -1,50 +1,76 @@
 //Express server
-'use strict'
+'use strict';
 
-//create app
+//create app, import express
 var express = require('express');
 var app = express();
+app.set('port', process.env.PORT || 3000);
 
 //import self defined module(s)
-var fortune = require('./lib/fortune.js')
+var fortune = require('./lib/fortune.js');
 
-//set up handlebars view engine - handles HTML
+//set up handlebars view engine - handles HTML, import handlebars
 var handlebars = require('express3-handlebars').create({
     defaultLayout: 'main'
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', process.env.PORT || 3000);
-
 
 //set up static files directory
 app.use(express.static(__dirname + '/public'));
 
-//homepage route
+//page testing with mocha
+app.use(function(req, res, next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+/*-----------------------------Defining route----------------------- */
+
+//routes homepage, app.get()
 app.get('/', function (req, res) {
     res.render('home');
 });
 
-//about page route
+//routes about page, app.get()
 app.get('/about', function (req, res) {
-    res.render('about', {fortune: fortune.getFortune()});
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
 });
 
+//routes contact page, app.get()
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
 
-//custom 404 page
+//routes hood-river page, app.get()
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+
+//routes contact page, app.get()
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
+});
+
+//custom 404 page, app.use()
 app.use(function (req, res) {
     res.status(404);
     res.render('404');
 });
 
-//custom 500 page
+//custom 500 page, app.use()
 app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-//listening port
+if( app.thing == null ) console.log( 'bleat!' );
+
+//listening at port
 app.listen(app.get('port'), function () {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate...')
-})  
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate...');
+});
---
+++ site/public/qa/tests-global.js
@@ -0,0 +1,8 @@
+'use strict';
+
+suite('Global Tests', function () {
+    test('page has a valid title', function () {
+        assert(document.title && document.title.match(/\S/) &&
+            document.title.toUpperCase() !== 'TODO');
+    });
+});

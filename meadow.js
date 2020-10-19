189631ad364e4d95c77b6c4d4d2f77f522c58be3
Monsur <donkenny03@gmail.com>
2020-10-19 05:12:58 +0100

    Files added after env state configured

--- meadow.js
+++ meadow.js
@@ -4,21 +4,133 @@
 //create app, import express
 var express = require('express');
 var app = express();
+
+//create app, import express
 app.set('port', process.env.PORT || 3000);
+
+//listening at port
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + ' at ' + app.get('env') + ' environment; press Ctrl-C to terminate...');
+});
+
+
+function startServer() {
+    //create app, import express
+    var express = require('express');
+    var app = express();
+    //create app, import express
+    app.set('port', process.env.PORT || 3000);
+
+    //listening at port
+    app.listen(app.get('port'), function () {
+        console.log('Express started on http://localhost:' + app.get('port') + ' at ' + app.get('env') + ' environment; press Ctrl-C to terminate...');
+    });
+}
+
+if (require.main === module) {
+    // application run directly; start app server
+    console.log('[EXPRESS STARTED FROM MAIN]')
+    //startServer();
+} else {
+    // application imported as a module via "require": export function
+    // to create server
+    module.exports = startServer;
+    console.log('[EXPRESS STARTED FROM CLUSTER]')
+}
+
+//worker or master?  
+/*
+app.use(function (req, res, next) {
+    var cluster = require('cluster');
+    if (cluster.isWorker) console.log('Worker %d received request',
+        cluster.worker.id);
+});
+*/
 
 //middleware for handling file upload
 var formidable = require('formidable');
 
-//middleware for JQuery file upload
+//middleware for  JQuery file uploa
 var jqupload = require('jquery-file-upload-middleware');
 
 //import self defined module(s)
 var fortune = require('./lib/fortune.js');
 var weatherData = require('./lib/weather-data.js');
 var credentials = require('./lib/credentials.js');
+var isFound = require('./lib/drive-detector.js');
 
 //import middleware for handling form body
 app.use(require('body-parser')());
+
+// // //email server
+// // var nodemailer = require('nodemailer');
+
+// // var mailTransport = nodemailer.createTransport('SMTP', {
+// //     host: 'smtp.meadowlarktravel.com',
+// //     secureConnection: true, // use SSL
+// //     port: 465,
+// //     auth: {
+// //         user: credentials.meadowlarkSmtp.user,
+// //         pass: credentials.meadowlarkSmtp.password,
+// //     }
+// // });
+
+// // //send mail
+// // mailTransport.sendMail({
+// //     from: '"Meadowlark Travel" <info@meadowlarktravel.com>',
+// //     to: 'joecustomer@gmail.com',
+// //     subject: 'Your Meadowlark Travel Tour',
+// //     text: 'Thank you for booking your trip with Meadowlark Travel. ' +
+// //         'We look forward to your visit!',
+// // }, function (err) {
+// //     if (err) console.error('Unable to send email: ' + error);
+// // });
+
+//uncaught exception, domains
+app.use(function (req, res, next) {
+    // create a domain for this request
+    var domain = require('domain').create();
+    // handle errors on this domain
+    domain.on('error', function (err) {
+        console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+        try {
+            // failsafe shutdown in 5 seconds
+            setTimeout(function () {
+                console.error('Failsafe shutdown.');
+                process.exit(1);
+            }, 5000);
+            // disconnect from the cluster
+            var worker = require('cluster').worker;
+            if (worker) worker.disconnect();
+            // stop taking new requests
+            server.close();
+            try {
+                // attempt to use Express error route
+                next(err);
+            } catch (err) {
+                // if Express error route failed, try
+                // plain Node response
+                console.error('Express error mechanism failed.\n', err.stack);
+                res.statusCode = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Server error.');
+            }
+        } catch (err) {
+            console.error('Unable to send 500 response.\n', err.stack);
+        }
+    });
+    // add the request and response objects to the domain
+    domain.add(req);
+    domain.add(res);
+    // execute the rest of the request chain in the domain
+    domain.run(next);
+});
+
+// // other middleware and routes go here
+// var server = http.createServer(app).listen(app.get('port'), function () {
+//     console.log('Listening on port %d.', app.get('port'));
+// });
+
 
 //import middleware for handling cookies
 app.use(require('cookie-parser')(credentials.cookieSecret));
@@ -57,7 +169,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-//middleware to inject data
+//middleware to inject data, from weatherview
 app.use(function (req, res, next) {
     if (!res.locals.partials) res.locals.partials = {};
     res.locals.partials.weather = weatherData.getWeatherData();
@@ -73,6 +185,22 @@ app.use(function (req, res, next) {
     next();
 });
 
+//environment set up
+switch (app.get('env')) {
+    case 'development':
+        // compact, colorful dev logging
+        app.use(require('morgan')('dev'));
+        console.log('Running mode: DEV_MODE');
+        break;
+    case 'production':
+        // module 'express-logger' supports daily log rotation
+        app.use(require('express-logger')({
+            path: __dirname + '/log/requests.log'
+        }));
+        console.log('Running mode: PROD_MODE');
+        break;
+}
+
 /*-----------------------------Defining route----------------------- */
 
 //routes homepage, app.get()
@@ -80,10 +208,24 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+//routes for exception
+app.get('/fail', function (req, res) {
+    throw new Error('Nope!');
+});
+
+//routes drive checker, 
+//routes homepage, app.get()
+app.get('/check-drive', function (req, res) {
+    res.render('check-drive', {
+        driveState: 'd'
+    });
+});
+
 //routes robots, app.get()
 app.get('/robots', function (req, res) {
     res.type('text/plain');
-    res.render('partials/robots');
+    res.render('layouts/robots',
+        { layout: 'robots' });
 });
 
 //routes about page, app.get()`
@@ -101,7 +243,6 @@ app.get('/contact', function (req, res) {
 
 //routes hood-river page, app.get()
 app.get('/tours/hood-river', function (req, res) {
-    if (req.query.email && req.query.name && req.query.groupSize) { }
     res.render('tours/hood-river',
         {
             email: req.query.email,
@@ -127,7 +268,7 @@ app.get('/newsletter', function (req, res) {
     res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 
-//newsletter form handling
+//newsletter form handling, (posted to newsletter)
 app.post('/newsletter', function (req, res) {
     var name = req.body.name || '', email = req.body.email || '';
     // input validation
@@ -138,30 +279,30 @@ app.post('/newsletter', function (req, res) {
             intro: 'Validation error!',
             message: 'The email address you entered was not valid.',
         };
-        return res.redirect(303, '/newsletter/archive');
+        return res.redirect(303, '/newsletter');
     }
-    new NewsletterSignup({ name: name, email: email }).save(function (err) {
-        if (err) {
-            if (req.xhr) return res.json({ err: 'Database error.' });
-            req.session.flash = {
-                type: 'danger',
-                intro: 'Database error!',
-                message: 'There was a database error; please try again later.'
-            }
-            return res.redirect(303, '/newsletter/archive');
-        }
-        if (req.xhr) return res.json({ success: true });
-        req.session.flash = {
-            type: 'success',
-            intro: 'Thank you!',
-            message: 'You have now been signed up for the newsletter.',
-        };
-        return res.redirect(303, '/newsletter/archive');
-    });
+    // new NewsletterSignup({ name: name, email: email }).save(function (err) {
+    //     if (err) {
+    //         if (req.xhr) return res.json({ err: 'Database error.' });
+    //         req.session.flash = {
+    //             type: 'danger',
+    //             intro: 'Database error!',
+    //             message: 'There was a database error; please try again later.'
+    //         }
+    //         return res.redirect(303, '/newsletter/archive');
+    //     }
+    if (req.xhr) return res.json({ success: true });
+    req.session.flash = {
+        type: 'success',
+        intro: 'Thank you!',
+        message: 'You have now been signed up for the newsletter.',
+    };
+    return res.redirect(303, '/newsletter');
 });
 
 
-//form handling, post method
+
+//form handling, post method for newsletter (posted to process)
 app.post('/process', function (req, res) {
     console.log('Form (from querystring): ' + req.query.form);
     console.log('CSRF token (from hidden form field): ' + req.body._csrf);
@@ -194,11 +335,12 @@ app.get('/contest/vacation-photo', function (req, res) {
     });
 });
 
-//form handling for file type
+//form handling for file type using formidable middleware
 app.post('/contest/vacation-photo/:year/:month', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         if (err) return res.redirect(303, '/error');
+        fields.path = "C:\\Users\\User\\Desktop"
         console.log('received fields:');
         console.log(fields);
         console.log('received files:');
@@ -226,9 +368,14 @@ app.use('/upload', function (req, res, next) {
 //Error handling--------------------------------------------
 //custom 404 page, app.use()
 app.use(function (req, res) {
-    res.status(404);
-    res.render('404');
+    res.status(404).render('404');
 });
+
+/*app.get('/epic-fail', function (req, res) {
+    process.nextTick(function () {
+        throw new Error('Kaboom!');
+    });
+});*/
 
 //custom 500 page, app.use()
 app.use(function (err, req, res, next) {
@@ -237,9 +384,4 @@ app.use(function (err, req, res, next) {
     res.render('500');
 });
 
-if (app.thing == null) console.log('bleat!');
 
-//listening at port
-app.listen(app.get('port'), function () {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate...');
-});
